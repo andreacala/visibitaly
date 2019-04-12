aef95031890caa246c2d320f41a66cd9d361f909
Andrea Calaminici <calaminici@outlook.com>
2019-04-12 18:24:55 +0200

    end of day

---
+++ Shuttle2Airport/codice_php_interno_contact.php
@@ -0,0 +1,57 @@
+ <?php
+
+
+
+    adulti = $_POST{'adulti'};
+    bambini = $_POST{'bambini'};
+
+
+    if ($_POST{'singolo-andataritorno'} == "Singolo")
+     {
+            switch (adulti) 
+            {
+                case 1:
+                    c_iniziale = 27;
+                    break;
+                case 2:
+                    c_iniziale = 52;
+                    break;
+                case 3:
+                    c_iniziale = 69;
+                    break;
+                case 4:
+                    c_iniziale =88;
+                    break;
+                case 5:
+                    c_iniziale =100;
+                    break;
+                case 6:
+                    c_iniziale =108;
+                    break;
+                case 7:
+                    c_iniziale=112;
+                    break;
+                case 8:
+                    c_iniziale=116;
+                    break;
+            }
+
+
+            
+
+
+
+
+
+
+
+        }
+
+
+    if($_POST {' singolo-andataritorno'}=="Andata e ritorno") { }
+    
+    risultato=c_iniziale;
+    
+    
+    
+    return(risultato); ?>
